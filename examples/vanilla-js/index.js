ebf6cca6646fabec0e2da6fc885310403588a001
Piyush <piyush.jain@flamapp.com>
2022-08-18 16:39:07 +0530

    fix bg issue

--- examples/vanilla-js/index.js
+++ examples/vanilla-js/index.js
@@ -1,7 +1,6 @@
 const buyBtn = document.querySelector("#placeorder");
 
 buyBtn.addEventListener("click", () => {
-    console.log("Call show popup")
     const flam = new FlamSDK();
 
     flam.placeOrder("abc");
--- sdk/ui-2/pages/index.tsx
+++ sdk/ui-2/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Card from '../components/atoms/Card'
 import FlexCenter from '../components/atoms/FlexCenter'
 import {IoIosCloseCircleOutline} from "react-icons/io";
+import Text from '../components/atoms/Text';
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,8 @@ const Home: NextPage = () => {
         <Card className='relative'>
           <div>Hello</div>
           <IoIosCloseCircleOutline className='absolute h-8 w-8 right-0 -top-10 cursor-pointer' />
+          {/* PIYUSH CODE */}
+          <Text>Upload Video</Text>
         </Card>
       </FlexCenter>
     </div>
