8bf826e69d7d2a80eb4e91e85f7f3ed0a23a1460
Yuvraj Singh Chouhan <yuvrajisbest13@gmail.com>
2022-08-18 17:30:48 +0530

    Fix commits

--- sdk/ui/pages/index.tsx
+++ sdk/ui/pages/index.tsx
@@ -2,11 +2,8 @@ import type { NextPage } from 'next'
 import Card from '../components/atoms/Card'
 import FlexCenter from '../components/atoms/FlexCenter'
 import {IoIosCloseCircleOutline} from "react-icons/io";
-<<<<<<< Updated upstream:sdk/ui-2/pages/index.tsx
 import Text from '../components/atoms/Text';
-=======
 import useMessage from '../hooks/useMessage';
->>>>>>> Stashed changes:sdk/ui/pages/index.tsx
 
 const Home: NextPage = () => {
 
@@ -17,13 +14,9 @@ const Home: NextPage = () => {
       <FlexCenter>
         <Card className='relative'>
           <div>Hello</div>
-<<<<<<< Updated upstream:sdk/ui-2/pages/index.tsx
           <IoIosCloseCircleOutline className='absolute h-8 w-8 right-0 -top-10 cursor-pointer' />
-          {/* PIYUSH CODE */}
           <Text>Upload Video</Text>
-=======
           <IoIosCloseCircleOutline className='absolute h-8 w-8 right-0 -top-10 cursor-pointer' onClick={() => sendMessage({type: "close"})} />
->>>>>>> Stashed changes:sdk/ui/pages/index.tsx
         </Card>
       </FlexCenter>
     </div>
