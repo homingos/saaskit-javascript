3ccfe3e3e3ac2ffdb4768533dcbfd3af63aaaf90
Yaksh Chopra <yakshchopra@gmail.com>
2022-09-01 17:21:25 +0530

    video later fix

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,7 +1,5 @@
 import '../styles/globals.scss';
 import '../styles/tooltip.scss';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 
 import type { AppProps } from 'next/app';
 
