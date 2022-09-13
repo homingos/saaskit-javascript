40b8da11726e33ea60408938c7dbae13af2d6c5a
Ritesh Bucha <bucharitesh@gmail.com>
2022-09-13 18:58:40 +0530

    feat(UI): seperate UI

--- ui/components/atoms/Scrollable.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Scrollable: React.FC<{ children: React.ReactNode, vertical?: boolean, horizontal?: boolean, className?: string }> = ({ children, vertical = true, horizontal = false, className = "" }) => {
-    return <div className={`custom-scrollbar ${vertical && "overflow-y-auto"} ${horizontal && "overflow-x-auto pb-2"} ${className}`}>{children}</div>
-}
-
-export default Scrollable;
