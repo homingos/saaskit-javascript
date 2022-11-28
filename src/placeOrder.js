fd050de4ad30ab3f7bb02865112e55974f09c16c
Yuvraj Singh Chouhan <yuvrajisbest13@gmail.com>
2022-11-28 12:42:10 +0530

    Test for YC

--- src/placeOrder.js
+++ src/placeOrder.js
@@ -25,133 +25,16 @@ export default async function placeOrder(order_details, callback) {
         message: "'order details' is not valid."
       },
       {
-        productId: {
+        first_name: {
           type: 'string',
-          message: "'productId' is required string."
+          message: "'first_name' is required string."
         },
-        variantId: {
+        last_name: {
           type: 'string',
-          message: "'variantId' is required string."
-        },
-        refId: { type: 'string', message: "'refId' is required string." },
-        orderId: {
-          optional: true,
-          type: 'string',
-          message: "'orderId' must be string."
-        },
-        photo: {
-          optional: true,
-          type: 'string',
-          message: "'photo' must be string."
-        },
-        allowVideoLater: {
-          optional: true,
-          type: 'boolean',
-          message: "'allowVideoLater' must be a boolean."
-        },
-        showPreview: {
-          optional: true,
-          type: 'boolean',
-          message: "'showPreview' must be a boolean."
-        },
-        video: {
-          optional: true,
-          type: 'string',
-          message: "'video' must be string."
-        },
-        prefill: {
-          optional: true,
-          type: 'object',
-          message: "'prefill' must be object."
-        },
-        animation: {
-          optional: true,
-          type: 'string',
-          message: "'animation' must be string."
-        },
-        theme: {
-          optional: true,
-          type: 'object',
-          message: "'theme' must be object."
-        },
-        logo: {
-          optional: true,
-          type: 'string',
-          message: "'logo' must be string."
+          message: "'last_name' is required string."
         }
       }
     );
-
-    // validate prefill
-    if (order_details.prefill) {
-      assert.check(
-        order_details.prefill,
-        {
-          type: 'object',
-          message: "'prefill' is not valid."
-        },
-        {
-          hide: {
-            optional: true,
-            type: 'boolean',
-            message: "'hide' must be boolean."
-          },
-          name: {
-            optional: true,
-            type: 'string',
-            message: "'name' mus be string."
-          },
-          email: {
-            optional: true,
-            type: 'string',
-            message: "'email' mus be string."
-          },
-          phone: {
-            optional: true,
-            type: 'string',
-            message: "'phone' must be string."
-          }
-        }
-      );
-    }
-
-    // validate theme
-    if (order_details.theme) {
-      assert.check(
-        order_details.theme,
-        {
-          type: 'object',
-          message: "'theme' is not valid."
-        },
-        {
-          primaryColor: {
-            optional: true,
-            type: 'string',
-            message: "'primary' must be string."
-          }
-        },
-        {
-          secondaryColor: {
-            optional: true,
-            type: 'string',
-            message: "'secondary' must be string."
-          }
-        },
-        {
-          headerText: {
-            optional: true,
-            type: 'string',
-            message: "'headerText' must be string."
-          }
-        }
-      );
-    }
-
-    // validate callback function
-    assert.check(callback, {
-      type: 'function',
-      message: "'callback' is required function."
-    });
 
     // save order_details
     this.order_details = order_details;
