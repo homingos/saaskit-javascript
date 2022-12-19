1e3235a64bf0c61e744b61bad9112a175a3e412a
Ritesh Bucha <bucharitesh@gmail.com>
2022-12-19 17:48:41 +0530

    chore(example): add random uuid generator for ref id

--- examples/vanilla-js/index.js
+++ examples/vanilla-js/index.js
@@ -3,11 +3,20 @@ const sdk = new FlamSaasSDK.init({
   key: '5dcac254-4b87-4ef7-96fe-b79cecdd54cf'
 });
 
+function uuidv4() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
+
 function launchSDK() {
   const data = {
     productId: 'a623e6a2-b366-4fb0-b193-ec1ad2d36b5d',
     varientId: 'a623e6a2-b366-4fb0-b193-ec1ad2d36b5d',
-    refId: '1234poiuq',
+    refId: uuidv4(),
     photo: {
       changable: true,
       url: 'https://plus.unsplash.com/premium_photo-1661962407604-711dda73e4d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60',
