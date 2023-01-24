aa6cd51010af4dcc7f8bc3e35eb93aa0fab30d1e
Yuvraj Singh Chouhan <yuvrajisbest13@gmail.com>
2023-01-24 12:03:12 +0530

    Fix(lib): fix ui link

--- dist/FlamSaasSDK.js
+++ dist/FlamSaasSDK.js
@@ -1,7 +1,7 @@
 /**
  * flamsdk v2.0.0
  * Author: bucharitesh
- * Date: 2023-01-19
+ * Date: 2023-01-24
  * License: MIT
  */
 
@@ -38,7 +38,10 @@
 
   const handleSend = message => {
     const iframe = document.getElementById('flam-sdk-iframe');
-    iframe.contentWindow.postMessage(message, 'http://localhost:3000');
+    iframe.contentWindow.postMessage(
+      message,
+      'https://zingcam-sdk-v2-dev.vercel.app'
+    );
   };
 
   const renderIframe = () => {
@@ -120,7 +123,7 @@
     const body = document.querySelector('body');
     const wrapper = document.createElement('div');
     wrapper.id = 'flam-sdk-wrapper';
-    wrapper.innerHTML = `<iframe id="flam-sdk-iframe" style="display: none" name="flam-sdk-iframe" src="http://localhost:3000" style="opacity: 0"></iframe>`;
+    wrapper.innerHTML = `<iframe id="flam-sdk-iframe" style="display: none" name="flam-sdk-iframe" src="https://zingcam-sdk-v2-dev.vercel.app" style="opacity: 0"></iframe>`;
     body.appendChild(wrapper);
   };
 
